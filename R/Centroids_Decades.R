5fe577ad6c9d0fd8cbc3e36e54fcbf50a9890242
Omar Daniel Leon Alvarado <leon.alvarado12@gmail.com>
2025-08-13 22:59:40 -0400

    Update Centroids_Decades.R

--- R/Centroids_Decades.R
+++ R/Centroids_Decades.R
@@ -205,3 +205,143 @@ c_coords
 #write.csv(c_coords, file = "Data/Centroids_Data_Coords.csv", quote = F, row.names = F)
 write.csv(c_coords, file = "Data/Centroids_Data_CoordsYEARS.csv", quote = F, row.names = F)
 
+
+###############################################################################
+
+          ##########################################################
+          #                Again for U.S. territory                #
+          ##########################################################
+
+###############################################################################
+
+
+dataIn <- read.csv("Data/Occurences_Tb_TempPeriods_FINAL.csv")
+head(dataIn)
+
+albers_proj <- st_crs("+proj=aea +lat_1=29.5 +lat_2=45.5 +lon_0=-96 +datum=WGS84")
+
+Series <- 1:length(unique(dataIn$cat2))
+Series <- Series[-c(1,2)] # remove first two series, few points
+Series # Centroid analysis will start since 1920
+
+Years <- sort(unique(dataIn$Year))
+Series <- 1:length(Years)
+Series <- Series[-c(1:29)]
+Years[Series]
+
+#################################
+#          FINAL CODE           #
+#################################
+
+#First, create the polygons for each period for each region
+# Then, calculate the centroids and area of each polygon
+
+
+## Lists for polygons
+
+USpol <- list()
+
+## List for centroids
+
+USc <- list()
+
+## List for areas
+
+USa <- list()
+
+######################
+
+US <- dataIn
+
+cumulative <- c()
+
+for(i in 1:length(Series)){
+  if(i==1){
+    #Extract only the specific period, this is an exception 1900-1920
+    period <- US[which(US$cat2%in%c(1,2,Series[i])), c('Lon','Lat')] ## FOR DECADES
+    #period <- US[which(US$Year%in%Years[c(1:29,Series[i])]),c('Lon','Lat')] ## FOR EACH YEAR
+
+    
+    period <- st_as_sf(period, coords = c("Lon", "Lat"), crs = 4326)
+    period <- st_transform(period, crs=albers_proj)
+    
+    USpol[[i]] <- st_convex_hull(st_union(period)) # Polygon
+    USc[[i]] <- st_centroid(st_convex_hull(st_union(period))) # Centroid
+    USa[[i]] <- (st_area(st_convex_hull(st_union(period))) / 1e6) #area in Km
+    
+    cumulative <- c(cumulative,1:2,Series[i]) ## FOR DECADES
+    #cumulative <- c(cumulative,1:29,Series[i]) ## FOR EACH YEAR
+    
+    
+  }else{
+    
+    #Extract only the specific period, this is an exception 1900-1920
+    period <- US[which(US$cat2%in%c(cumulative,Series[i])), c('Lon','Lat')] ## FOR DECADES
+    #period <- US[which(US$Year%in%Years[c(cumulative,Series[i])]),c('Lon','Lat')] ## FOR EACH YEAR
+    
+    period <- st_as_sf(period, coords = c("Lon", "Lat"), crs = 4326)
+    period <- st_transform(period, crs=albers_proj)
+    
+    USpol[[i]] <- st_convex_hull(st_union(period)) # Polygon
+    USc[[i]] <- st_centroid(st_convex_hull(st_union(period))) # Centroid
+    USa[[i]] <- (st_area(st_convex_hull(st_union(period))) / 1e6) #area in Km
+    
+    cumulative <- c(cumulative, Series[i])
+    
+  }
+}
+
+#############################################
+
+centroid_distUS <- c()
+
+for(i in 1:length(Series)){
+  if(i==1){
+    
+    centroid_distUS <- c(centroid_distUS, 0)
+
+  }else{
+    
+    distUS <- st_distance(USc[[i]],USc[[i-1]])
+    distUS <- as.numeric(distUS)/1000
+    
+    avgUS <- (USa[[i]] + USa[[i-1]]) / 2
+    
+    centroid_distUS <- c(centroid_distUS,(distUS / sqrt(avgUS)))
+    
+  }
+}
+
+centroid_distUS
+
+ggplot()+
+  geom_point(aes(x=1:length(Series), centroid_distUS))
+
+
+centroid_frame <- data.frame(US  = centroid_distUS)
+
+centroid_frame
+
+write.csv(centroid_frame, file = "Data/Centroids_Data_US.csv", quote = F, row.names = F)
+#write.csv(centroid_frame, file = "Data/Centroids_Data_USYEARS.csv", quote = F, row.names = F)
+
+################
+
+c_coords <- matrix(NA, nrow = 1, ncol = 2)
+colnames(c_coords) <- c("X","Y")
+
+for(i in 1:length(USc)){
+  
+  UScc <- st_transform(USc[[i]], crs = 4326)
+  
+  c_coords <- rbind(c_coords, 
+                    st_coordinates(UScc))
+  
+}
+
+c_coords <- c_coords[-1,]
+c_coords <- as.data.frame(cbind(Year=1:length(Series), c_coords))
+c_coords
+
+write.csv(c_coords, file = "Data/Centroids_Data_CoordsUS.csv", quote = F, row.names = F)
+#write.csv(c_coords, file = "Data/Centroids_Data_CoordsUSYEARS.csv", quote = F, row.names = F)
